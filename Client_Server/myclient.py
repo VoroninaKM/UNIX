ece4f65d62662007d08742d8523641d84e8b4b8e
Voronina Ksenia <63294575+kseniavv@users.noreply.github.com>
2021-03-08 14:37:04 +0300

    эхо-сервер

---
+++ Client_Server/myclient.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+
+sock = socket.socket()
+sock.connect(('192.168.1.45', 9090))
+print('Соединение с сервером прошло успешно!')
+sock.send('Hello, world!'.encode())
+print('Данные отправлены серверу.')
+
+data = sock.recv(1024)
+print('Прием данных прошел успешно.')
+print(data.decode())
+sock.close()
+print('Разрыв соединения с сервером.')
